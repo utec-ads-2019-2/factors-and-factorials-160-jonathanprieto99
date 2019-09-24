2b2d3bd920a6544fa1a909d8adf0680d6f341bc7
Jonathan Prieto <jonathan.prieto@me.com>
2019-09-24 14:05:58 -0500

    First Upload

---
+++ cmake-build-debug/CMakeFiles/factors-and-factorials-160-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/factors-and-factorials-160-jonathanprieto99.dir/main.cpp.o"
+  "factors-and-factorials-160-jonathanprieto99.pdb"
+  "factors-and-factorials-160-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/factors-and-factorials-160-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
