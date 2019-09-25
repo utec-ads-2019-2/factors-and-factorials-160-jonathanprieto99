a0e23626f3fe339d6aa50f0c903c35141496d617
Jonathan Prieto <jonathan.prieto@me.com>
2019-09-24 19:15:01 -0500

    OK

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <vector>
 #include <map>
 #include <iomanip>
+#include <array>
 
 using namespace std;
 
 int numerosprimos[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 int input;
-int carry[25];
+int carry[(sizeof(numerosprimos)/sizeof(*numerosprimos))];
 
 int main () {
 
